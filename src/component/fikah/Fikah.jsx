9c0a53fc10b411116d209a7ee8e307cbe9563e68
unknown <macherki.nessrine@yahoo.com>
2024-04-29 18:19:02 +0100

    fix fikah path

--- src/component/fikah/Fikah.jsx
+++ src/component/fikah/Fikah.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const Fikah = () => {
   return (
 <div className='grid grid-cols-3 gap-4 h-screen p-5 ml-5'>
-  <NavLink to="/zakat">
+  <NavLink to="Al-Zaytoona/zakat">
     <div className="w-72 h-64 bg-pink-500 flex justify-center items-center text-white rounded-xl">الزكاة</div>
   </NavLink>
   <div className="w-72 h-64 bg-blue-500 flex justify-center items-center text-white rounded-xl">الصلاة</div>
